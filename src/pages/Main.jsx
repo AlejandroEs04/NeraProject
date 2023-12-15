aa6ca4790ad5de439448a8e044d07f1087ba8b33
Alexanderel04 <2004.estrada.lopez@gmail.com>
2023-12-15 10:49:35 -0600

    "First Commit"

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,13 @@
+import logoimg from '../../public/img/logo.png';
 
 const Main = () => {
-  return (
-    <div>
-        Hola
-    </div>
-  )
+    return (
+      <div className="flex flex-col justify-center items-center">
+        <img src={logoimg} alt='logo NERA Group' className='max-w-sm mt-32' />
+
+        <h1 className='text-3xl font-bold text-white mt-5'>PROXIMAMENTE...</h1>
+      </div>
+    )
 }
 
 export default Main

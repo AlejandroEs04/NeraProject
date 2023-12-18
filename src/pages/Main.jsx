45ee4e7a763dd8f483a805e645fa9b7db8690506
Alexanderel04 <2004.estrada.lopez@gmail.com>
2023-12-18 14:15:40 -0600

    "Pre update 18/12/2023"

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,7 +5,7 @@ const Main = () => {
       <div className="flex flex-col justify-center items-center">
         <img src={logoimg} alt='logo NERA Group' className='max-w-sm mt-32' />
 
-        <h1 className='text-3xl font-bold text-white mt-5'>PROXIMAMENTE...</h1>
+        <h1 className='text-3xl font-bold text-white mt-5'>Coming soon...</h1>
       </div>
     )
 }
